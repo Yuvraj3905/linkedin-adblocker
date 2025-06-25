aa14412fe51823708b3b985519e7bc6b8a581881
Yuvraj3905 <yuvraj202001@gmail.com>
2025-06-25 20:08:41 +0530

    fix: some fixes for running it in latest chrome versions

--- extension.js
+++ extension.js
@@ -3,45 +3,24 @@
     As opposed to e.g. webNavigation.onCompleted, this will start to run early
     so that we can begin to remove ads as soon as possible.
 */
-chrome.webNavigation.onCommitted.addListener(function (tab) {
-    // Prevents script from running when other frames load
-    if (tab.frameId == 0) {
-        chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+chrome.webNavigation.onCommitted.addListener(async function (tab) {
+  // Prevents script from running when other frames load
+  if (tab.frameId == 0) {
+    const tabInfo = await chrome.tabs.get(tab.tabId);
+    const url = tabInfo.url;
+    // Remove unnecessary protocol definitions and www subdomain from the URL
+    const parsedUrl = url.replace(/^https?:\/\//, "").replace("www.", "");
 
-            // Get the URL of the webpage
-            let url = tabs[0].url;
-            // Remove unnecessary protocol definitions and www subdomain from the URL
-            let parsedUrl = url.replace("https://", "")
-                .replace("http://", "")
-                .replace("www.", "")
+    // Remove path and queries e.g. linkedin.com/feed or linkedin.com?query=value
+    // We only want the base domain
 
-            // Remove path and queries e.g. linkedin.com/feed or linkedin.com?query=value
-            // We only want the base domain
-            let domain = parsedUrl.slice(0, parsedUrl.indexOf('/') == -1 ? parsedUrl.length : parsedUrl.indexOf('/'))
-                .slice(0, parsedUrl.indexOf('?') == -1 ? parsedUrl.length : parsedUrl.indexOf('?'));
+    const domain = parsedUrl.split(/[/?#]/)[0];
 
-            try {
-                if (domain.length < 1 || domain === null || domain === undefined) {
-                    return;
-                } else if (domain == "linkedin.com") {
-                    runLinkedinScript();
-                    return;
-                }
-            } catch (err) {
-                throw err;
-            }
-
-        });
+    if (domain === "linkedin.com") {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.tabId },
+        files: ["linkedin.js"],
+      });
     }
+  }
 });
-
-
-function runLinkedinScript() {
-    // Inject script from file into the webpage
-    chrome.tabs.executeScript({
-        file: 'linkedin.js'
-    });
-    return true;
-}
-
-
